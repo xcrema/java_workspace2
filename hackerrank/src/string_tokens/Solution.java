75134bc368f9aef87d7ddf578cc7fe082247dc51
Achyut <achyut.terminator@gmail.com>
2020-03-27 13:36:18 +0530

    commit

--- hackerrank/src/string_tokens/Solution.java
+++ hackerrank/src/string_tokens/Solution.java
@@ -6,8 +6,14 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
+		s = s.trim();
+		if (s.length() == 0) {
+			System.out.println("0");
+		}
 		if (s.length() >= 1 && s.length() <= 400000) {
 			//String[] str = s.split("[A-Za-z !,?._'@]+");
+			
+			System.out.println(s);
 			String[] str = s.split("[!@,? '._]+");
 			System.out.println(str.length);
 			for (String word : str) {
